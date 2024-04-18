c2a43482c95281575268902b7a5f4a0b3697ff1f
dsdanhsa <letandanhqn@gmail.com>
2024-04-18 09:26:49 +0700

    sang 18 4 commit

--- dashboard/hrapp/urls.py
+++ dashboard/hrapp/urls.py
@@ -4,4 +4,9 @@ from . import views
 app_name = 'hrapp'
 
 urlpatterns = [
+    path('benefitplans/', views.Benefitplans_list, name='benefitplans'),
+    path('employmentworkingtime/', views.EmploymentWorkingTime_list, name='employmentworkingtime'),
+    path('employment/', views.Employment_list, name='employment'),
+    path('jobhistory/', views.Jobhistory_lisst, name='jobhistory'),
+    path('personal/', views.Personal_list, name='personal')
 ]
--- dashboard/hrapp/views.py
+++ dashboard/hrapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import BenefitPlans, Employment,EmploymentWorkingTime, JobHistory, Personal
 
-# Create your views here.
+@login_required
+def Benefitplans_list(request):
+    benefitplans = BenefitPlans.objects.all()
+    return render(request, 'hrapp/list_benefitplan.html', {'benefitplans': benefitplans})
+
+@login_required
+def Employment_list(request):
+    employments = Employment.objects.all()
+    return render(request, 'hrapp/list_employment.html', {'employments': employments})
+
+@login_required
+def EmploymentWorkingTime_list(request):
+    employmentworkingtimes = EmploymentWorkingTime.objects.all()
+    return render(request, 'hrapp/list_EmploymentWorkingTime.html', {'employmentworkingtimes': employmentworkingtimes})
+
+@login_required
+def Jobhistory_lisst(request):
+    jobhistorys = JobHistory.objects.all()
+    return render(request, 'hrapp/list_jobhistory.html', {'jobhistorys': jobhistorys})
+
+@login_required
+def Personal_list(request):
+    personals = Personal.objects.all()
+    return render(request, 'hrapp/list_personal.html', {'personals': personals})
