3e3b239db34b3626bb212a487ab6cb7a49d0762c
dsdanhsa <letandanhqn@gmail.com>
2024-05-22 18:45:19 +0700

    chieu 22 5

--- dashboard/static/js/home.js
+++ dashboard/static/js/home.js
@@ -1,19 +1,21 @@
-
-    const hour = document.getElementById('hour');
+const hour = document.getElementById('hour');
     const min = document.getElementById('min');
     const sec = document.getElementById('sec');
 
-    const clock = setInterval(function time() {
+    function updateTime() {
         let today = new Date();
         let hr = today.getHours();
         let mn = today.getMinutes();
         let se = today.getSeconds();
 
         if (hr < 10) hr = '0' + hr;
-        if (mn < 10) min = '0' + mn;
+        if (mn < 10) mn = '0' + mn;
         if (se < 10) se = '0' + se;
 
         hour.textContent = hr;
         min.textContent = mn;
         sec.textContent = se;
-    })
+    }
+
+    setInterval(updateTime, 1000);
+    updateTime();
